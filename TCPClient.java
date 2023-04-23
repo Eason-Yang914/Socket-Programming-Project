dec508e6a08bf140e3a694dc20d8a6a52aae0b21
Eason-Yang914 <chengkaiyang914@gmail.com>
2023-04-22 18:06:21 -0700

    Still working on Option 2 and 3

--- TCPClient.java
+++ TCPClient.java
@@ -1,87 +1,75 @@
-import java.io.*; 
+import java.io.*;
 import java.net.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 class TCPClient {
-   public static void main(String argv[]) throws Exception 
-   {
-      String username;
-      String password;
-      String statusChecking;
-      int userOption;
-      //String modifiedSentence;
-      // Get the buffer from the reader
-//      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-//      // create new socket
-//      Socket clientSocket = new Socket("127.0.0.1", 6789);
-//      //Date output
-//      DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-//      // Get the inputBuffer
-//      BufferedReader inFromServer = new BufferedReader(new InputStreamReader( clientSocket.getInputStream()));
-      boolean working = true;
-      //HashMap<String, String> userMap = new HashMap<>();
+    public static void main(String argv[]) throws Exception {
 
-      while(working){
+        boolean working = true;
 
-         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-         // create new socket
-         Socket clientSocket = new Socket("127.0.0.1", 6789);
-         //Date output
-         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-         // Get the inputBuffer
-         BufferedReader inFromServer = new BufferedReader(new InputStreamReader( clientSocket.getInputStream()));
-         // print options
-         System.out.println("0.Connect to the server\n"
-                              + "1.Get the user list\n"
-                              + "2.Send a message\n"
-                              + "3.Get my messages\n"
-                              + "4.Exit");
-         //user chose which one
-         System.out.println("Please enter one number:");
-         Scanner input = new Scanner(System.in);
-         userOption = input.nextInt();
+        // get buffer from user
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        // create new socket
+        Socket clientSocket = new Socket("127.0.0.1", 6789);
+        //Date output
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        // Get the inputBuffer
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-         if(userOption == 0) {
-            System.out.println("Please enter your name: ");
-            username = inFromUser.readLine();
-            outToServer.writeBytes(username + '\n');
 
-            System.out.println("Please enter your password: ");
-            password = inFromUser.readLine();
-            outToServer.writeBytes(password + '\n');
+        while (working) {
 
-            statusChecking = inFromServer.readLine();
-            System.out.println("From Server:"+ statusChecking+'\n');
+            // print options
+            System.out.println("0.Connect to the server\n"
+                    + "1.Get the user list\n"
+                    + "2.Send a message\n"
+                    + "3.Get my messages\n"
+                    + "4.Exit");
+            //user chose which one
+            System.out.println("Please enter one number:");
+            String option = inFromUser.readLine();
+            outToServer.writeBytes(option + "\n");
 
-            
-         } else if (userOption == 1){
-            outToServer.writeBytes("get_user_list\n");
+            //switch
+            switch (option) {
+                case "0":
+                    System.out.println("Please enter your name: ");
+                    String username = inFromUser.readLine();
+                    outToServer.writeBytes(username + "\n");
 
-            // Receive list of usernames from server
-            String userListString = inFromServer.readLine();
-            String[] userList = userListString.split(",");
+                    System.out.println("Please enter your password: ");
+                    String password = inFromUser.readLine();
+                    outToServer.writeBytes(password + "\n");
 
-            // Print list of usernames to user
-            System.out.println("List of usernames:");
-            for (String userName : userList) {
-               System.out.println(userName);
+                    String statusChecking = inFromServer.readLine();
+                    System.out.println("From Server:" + statusChecking + '\n');
+                    break;
+
+                case "1":
+                    System.out.println("All User List is:");
+                    String user = inFromServer.readLine();
+                    System.out.println(user);
+                    break;
+
+                case "2":
+                    System.out.println("Please enter the receiver's username: ");
+                    String receiver = inFromUser.readLine();
+                    System.out.println("Enter the message you want to leave: ");
+                    String message = inFromUser.readLine();
+
+                    outToServer.writeBytes(receiver + "\n");
+                    outToServer.writeBytes(message + "\n");
+                    break;
+
+                case "3":
+                    System.out.println("Your messages:");
+                    String answer = inFromServer.readLine();
+                    System.out.println(answer);
+                    break;
+
+                case "4":
+                    clientSocket.close();
+
             }
-
-         } else if (userOption == 2){
-
-         } else if (userOption == 3){
-
-         } else if (userOption == 4){
-            clientSocket.close();
-         }
-      }
-      //sentence = inFromUser.readLine();
-      //outToServer.writeBytes(sentence + '\n');
-      //username = inFromServer.readLine();
-      //password = inFromServer.readLine();
-      //System.out.println("FROM SERVER: " + modifiedSentence);
-      //clientSocket.close();
-   } 
+        }
+    }
 }
--- TCPServer.java
+++ TCPServer.java
@@ -1,54 +1,77 @@
-import java.io.*; 
+import java.io.*;
 import java.net.*;
 import java.util.*;
+
 class TCPServer {
-   public static void main(String argv[]) throws Exception
-   {
-      String clientUsername;
-      String clientPassword;
-      String clientMessage;
-      // create new hashMap
-      HashMap<String, String> map1 = new HashMap<>();
-      map1.put("Alice","1234");
-      map1.put("Bob","5678");
+    public static void main(String[] argv) throws Exception {
+        // create new hashMap
+        HashMap<String, String> map1 = new HashMap<>();
+        map1.put("Alice", "1234");
+        map1.put("Bob", "5678");
+        List<String> userNames = new ArrayList<String>(map1.keySet());
 
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
-      System.out.println("SERVER is running ... ");
+        while (true) {
+            ServerSocket welcomeSocket = new ServerSocket(6789);
+            System.out.println("SERVER is running ... ");
 
-      while(true) {
-         Socket connectionSocket = welcomeSocket.accept();
-         BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
-         DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-         clientUsername = inFromClient.readLine();
-         clientPassword = inFromClient.readLine();
-         System.out.println("UserName is: " + clientUsername);
-         System.out.println("UserPassword: " + clientPassword);
+            while (true) {
+//                Socket connectionSocket = welcomeSocket.accept();
+//                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+//                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                String option = inFromClient.readLine();
 
 
-         if(map1.containsKey(clientUsername) && map1.containsValue(clientPassword)){
-            System.out.println("Access Granted");
-            //System.out.println(inFromServer);
-            outToClient.writeBytes("Access Granted\n");
-         } else {
-            System.out.println("Access Denied – Username/Password Incorrect");
-            outToClient.writeBytes("Access Denied – Username/Password Incorrect\n");
-         }
+                switch (option) {
+                    case "0":
+                        String clientUsername = inFromClient.readLine();
+                        String clientPassword = inFromClient.readLine();
+                        System.out.println("The user's option is:" + option);
+                        System.out.println("UserName is: " + clientUsername);
+                        System.out.println("UserPassword: " + clientPassword);
+                        if (map1.containsKey(clientUsername) && map1.containsValue(clientPassword)) {
+                            System.out.println("Access Granted");
+                            //System.out.println(inFromServer);
+                            outToClient.writeBytes("Access Granted\n");
+                        } else {
+                            System.out.println("Access Denied – Username/Password Incorrect");
+                            outToClient.writeBytes("Access Denied – Username/Password Incorrect\n");
+                        }
 
-//         if (clientMessage.equals("get_user_list")) {
-//            // Send list of usernames to client
-//            StringBuilder userListBuilder = new StringBuilder();
-//            for (String username : map1.keySet()) {
-//               userListBuilder.append(username);
-//               userListBuilder.append(",");
-//            }
-//            String userListString = userListBuilder.toString();
-//            outToClient.writeBytes(userListString + "\n");
-//         }
+                        break;
+                    case "1":
+//                        System.out.println("The option is:" + option);
+//                        outToClient.writeBytes("Alice Bob\n");
+//                        System.out.println("Alice Bob");
+//                        break;
+                        System.out.println("The user's option is:" + option);
+                        String userList = String.join(" ", userNames);
+                        outToClient.writeBytes(userList + "\n");
+                        System.out.println("All User List is: " + userList);
+                        break;
 
+                    case "2":
+                        System.out.println("The user's option is:" + option);
+                        String receiver = inFromClient.readLine();
+                        System.out.println("The receiver is " + receiver);
+                        String message = inFromClient.readLine();
+                        System.out.println("The message is " + message);
+                        break;
 
+                    case "3":
+                        System.out.println("The user's option is:" + option);
+                        outToClient.writeBytes("\n");
+                        break;
 
-         //capitalizedSentence = clientSentence.toUpperCase() + '\n'; outToClient.writeBytes(capitalizedSentence);
-      } 
-   }
+                    case "4" : {
+                        connectionSocket.close();
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
